4cb97871ca713867ae33e6ce549ab831dc108126
Jennifer Hong <hongjenn24@gmail.com>
2021-12-01 16:27:55 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form');
+    // create an event listener that allows us to submit
+    
+    inputForm.addEventListener('submit', (event) => {
+        // create HTML tags that allow us to reference to index.html (where we want our inputs to show up)
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+        console.log(input.value);
 
+        // request data from the URL provided
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+          const title = document.querySelector('section#movieDetails h4');
+          const summary = document.querySelector('section#movieDetails p');
+    
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+    });
+  });
+}
+  
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
